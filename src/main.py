49744dd13fbb98aba1c7ec83389724bfc59cdbae
SangDoo Nam <kexe009@hotmail.com>
2023-11-08 22:43:12 +0100

    finish

--- src/main.py
+++ src/main.py
@@ -1,3 +1,10 @@
 from server_errors import OK, NotFound, ServerError
 
 # Write code to test your classes
+
+errors = [OK().status(200), NotFound().status(404), ServerError().status(500)]
+
+
+for error in errors:
+    if error:
+        print(error)
--- src/server_errors.py
+++ src/server_errors.py
@@ -2,12 +2,31 @@
 
 
 class OK:
-    pass
+    
+    def __init__(self) -> None:
+        self.error_code = 200
+        self.error_message = 'OK'
+    
+    def status(self, error_object):
+        if error_object == self.error_code:
+            return f'message: {self.error_message}: {self.error_code}'
 
 
-class NotFound:
-    pass
+class NotFound(OK):
+    def __init__(self) -> None:
+        self.error_code = 404
+        self.error_message = "This page does not exist"
+    
+    def status(self, error_object):
+        if error_object == self.error_code:
+            return f'error: {self.error_message }: {self.error_code}'
 
 
-class ServerError:
-    pass
+class ServerError(OK):
+    def __init__(self) -> None:
+        self.error_code = 500
+        self.error_message = "Internal Server Error"
+    
+    def status(self, error_object):
+        if error_object == self.error_code:
+            return f'error: {self.error_message }: {self.error_code}'
